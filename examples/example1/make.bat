783c7af96853042cc30bf2c8d311102719d42e6c
Adam Gates <adamgates84@gmail.com>
2013-03-25 15:26:09 +1100

    Move the examples out of the main build.

--- examples/example1/make.bat
+++ examples/example1/make.bat
@@ -2,8 +2,8 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 cd /d %~dp0
 
-set MYJAR=bin\RadConsole.jar
-
-xcopy /I /D libs\* bin
+set MYJAR=..\..\bin\Example1.jar
+set JAVA_LIBS=;..\..\libs\jna.jar;..\..\bin\RadConsole.jar
 
 call "%DROPBOX_DIR%\Current\Development\Java\javamake.bat" %*
+
