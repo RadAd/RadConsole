3d00690447c24f26bf29470b1dbec531c11b2fd1
Adam Gates <adamgates84@gmail.com>
2013-10-29 17:36:56 +1100

    Use a gist for the shared ant scripts.

--- make.bat
+++ make.bat
@@ -3,10 +3,4 @@ setlocal
 
 cd /d %~dp0
 
-rem set JM_PROJECT=RadConsole
-rem call ..\javamake.bat %*
-
-set JAVA_HOME=D:\Utils\Devel\jdk1.7_x86
-set ANT_HOME=D:\Utils\Devel\apache-ant
-
-call "%ANT_HOME%\bin\ant" %*
+call radjava\radjava.bat %*
